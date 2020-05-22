5d8787cf54ba77a239c095e520f7872e429949a8
= <=>
2020-05-22 20:12:59 +0700

    Comment file bị lỗi

--- Vhome/Test/test.js
+++ Vhome/Test/test.js
@@ -1,23 +1,23 @@
-class FlatlistItem extends Component() {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        const {onPress} =this.props
+// class FlatlistItem extends Component() {
+//     constructor(props) {
+//         super(props)
+//     }
+//     render() {
+//         const {onPress} =this.props
 
-        return (<TouchableOpacity onPress={onPress}>
-   <Image source={{ uri: this.props.item.image }}></Image>
-    <TouchableOpacity> )}
-    export default function Services() {
-   const { navigation } = this.props
-    return (
-        <View>
-            <FlatList data={flatListdata}
-                renderItem={({ item, index }) => {
-                    return (
-<FlatlistItem onPress={() => navigation.navigate('HealthCare')}  item={item} index={index}></FlatlistItem>
-                    )
-                }}></FlatList>
-        </View>
-    )
-}
+//         return (<TouchableOpacity onPress={onPress}>
+//    <Image source={{ uri: this.props.item.image }}></Image>
+//     <TouchableOpacity> )}
+//     export default function Services() {
+//    const { navigation } = this.props
+//     return (
+//         <View>
+//             <FlatList data={flatListdata}
+//                 renderItem={({ item, index }) => {
+//                     return (
+// <FlatlistItem onPress={() => navigation.navigate('HealthCare')}  item={item} index={index}></FlatlistItem>
+//                     )
+//                 }}></FlatList>
+//         </View>
+//     )
+// }
